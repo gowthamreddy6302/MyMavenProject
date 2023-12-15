ec0bb39ea96db81667d64a5f8b22bf2da85ce359
gowtham <agrss@gmail.com>
2023-12-15 10:12:45 +0530

    Updated changes on the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-
+        // SPRING CONTAINER
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:config.xml");
+        Customer cus = (Customer) context.getBean("customer");
+        System.out.println("CUSTOMER NAME :  " + cus.getName());
+        System.out.println("CUSTOMER CONTACT :  " + cus.getContact());
+        System.out.println("CUSTOMER ADDRESS :  " + cus.getAddress());
     }
 }
